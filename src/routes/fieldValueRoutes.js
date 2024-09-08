62321f43e78a6fdf745641bd920032a5973e81d3
A. Bryant <W.Sword.09@hotmail.com>
2024-09-08 00:01:36 -0600

    fix-fieldValueRoutes

--- src/routes/fieldValueRoutes.js
+++ src/routes/fieldValueRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const fieldValueController = require('../controllers/fieldValueController');
 const { authenticate, admin } = require('../middleware/auth');
 
-router.post('/fieldValues', authenticate, fieldValueController.createField);
-router.get('/fieldValues', authenticate, fieldValueController.getFields);
-router.get('/fieldValues/:id', authenticate, fieldValueController.getFieldById);
-router.put('/fieldValues/:id', authenticate, fieldValueController.updateField);
-router.delete('/fieldValues/:id', authenticate, fieldValueController.deleteField);
+router.post('/fieldValues', authenticate, fieldValueController.createFieldValue);
+router.get('/fieldValues', authenticate, fieldValueController.getFieldValues);
+router.get('/fieldValues/:id', authenticate, fieldValueController.getFieldValueById);
+router.put('/fieldValues/:id', authenticate, fieldValueController.updateFieldValue);
+router.delete('/fieldValues/:id', authenticate, fieldValueController.deleteFieldValue);
 
 module.exports = router;

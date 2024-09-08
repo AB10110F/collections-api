0e10225d9fc050f6b80668489f7c46c7a5eaa04b
A. Bryant <W.Sword.09@hotmail.com>
2024-09-07 23:56:59 -0600

    add-all-routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const sequelize = require('./config/database');
 const userRoutes = require('./routes/userRoutes');
 const collectionRoutes = require('./routes/collectionRoutes');
 const fieldRoutes = require('./routes/fieldRoutes');
+const fieldValueRoutes = require('./routes/fieldValueRoutes');
+const likeRoutes = require('./routes/likeRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const tagRoutes = require('./routes/tagRoutes');
 
 const app = express();
 
@@ -24,6 +28,11 @@ app.options('*', (req, res) => {
 app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/api', collectionRoutes);
+app.use('/api', fieldRoutes);
+app.use('/api', fieldValueRoutes);
+app.use('/api', likeRoutes);
+app.use('/api', commentRoutes);
+app.use('/api', tagRoutes);
 
 sequelize.sync()
   .then(() => {
@@ -37,4 +46,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-

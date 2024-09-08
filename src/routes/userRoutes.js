62ed711840746e7999c1632c18729a96895e66c7
A. Bryant <W.Sword.09@hotmail.com>
2024-09-08 00:00:31 -0600

    change-users-endpoints

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,13 +4,13 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 const { authenticate, admin } = require('../middleware/auth');
 
-router.post('/users', authenticate, userController.createUser);
+router.post('/users', userController.createUser);
 router.get('/users', authenticate, userController.getUsers);
 router.get('/users/:id', authenticate, userController.getUserById);
-router.put('/users/:id', authenticate, userController.updateUser);
-router.delete('/users/:id', authenticate, userController.deleteUser);
+router.put('/users', authenticate, userController.updateUser);
+router.delete('/users', authenticate, userController.deleteUser);
 
-router.put('/users/:id', authenticate, admin, userController.updateSudo);
+router.put('/users/sudo/:id', authenticate, admin, userController.updateSudo);
 
 router.post('/login', authController.login);
 
